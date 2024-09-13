bcb06ec08e46c05ede10bdb26b39c58a7fd04d78
Laita Zidane <laitazidane@gmail.com>
2024-09-13 11:11:18 +0700

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,7 +63,12 @@
                     </div>
 
                     <button type="submit"
-                        class="w-full text-white bg-red-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{{ __('Masuk') }}</button>
+                        class="w-full text-white bg-red-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        {{ __('Masuk') }}</button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            tidak punya akun? <a href="{{ route('register') }}" data-signup class="font-medium text-primary-600 hover:underline dark:text-primary-500">Daftar</a>
+                            </p>
+
                 </form>
             </div>
         </div>
