319b4f0525e318bbd5e77bbdae5e7cb0db2c138f
Laita Zidane <laitazidane@gmail.com>
2024-09-28 00:49:30 +0700

    logistik

--- app/Filament/Resources/LogistiksResource/Pages/CreateLogistiks.php
+++ app/Filament/Resources/LogistiksResource/Pages/CreateLogistiks.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateLogistiks extends CreateRecord
 {
     protected static string $resource = LogistiksResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
