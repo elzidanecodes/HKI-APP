69f47814268674678aba570a21d145b38258d0ce
Laita Zidane <laitazidane@gmail.com>
2024-09-28 00:23:33 +0700

    logistik

--- app/Filament/Resources/LogistiksResource.php
+++ app/Filament/Resources/LogistiksResource.php
@@ -6,10 +6,13 @@ use App\Filament\Resources\LogistiksResource\Pages;
 use App\Filament\Resources\LogistiksResource\RelationManagers;
 use App\Models\Logistiks;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -23,7 +26,28 @@ class LogistiksResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('nama_barang')
+                    ->label('Nama Barang')
+                    ->placeholder('Nama Barang')
+                    ->required(),
+                TextInput::make('kategori_barang')
+                    ->label('Kategori Barang')
+                    ->placeholder('Kategori Barang')
+                    ->required(),
+                TextInput::make('deskripsi_barang')
+                    ->label('Deskripsi Barang')
+                    ->placeholder('Deskripsi Barang')
+                    ->required(),
+                TextInput::make('jumlah_barang')
+                    ->label('Jumlah Barang')
+                    ->placeholder('Jumlah Barang')
+                    ->numeric() // Menandakan bahwa ini input angka
+                    ->minValue(0) // Batas minimal
+                    ->required(),
+                TextInput::make('nama_vendor')
+                    ->label('Nama Vendor')
+                    ->placeholder('Nama Vendor')
+                    ->required(),
             ]);
     }
 
@@ -31,7 +55,31 @@ class LogistiksResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('No')->getStateUsing(
+                    static function ( $rowLoop, HasTable $livewire): string {
+                        return (string) (
+                            $rowLoop->iteration +
+                            ($livewire->tableRecordsPerPage * (
+                                $livewire->page - 1
+                            ))
+                        );
+                    }
+                ),
+                TextColumn::make('nama_barang')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('kategori_barang')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('deskripsi_barang') 
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('jumlah_barang') 
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('nama_vendor') 
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
                 //
